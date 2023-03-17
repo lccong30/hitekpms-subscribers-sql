81be8d090c978debdf8caea3a3ae04c3b887167b
lccong30 <conglc3010@gmail.com>
2023-03-17 16:04:47 +0700

    adfjak

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 require("dotenv").config;
 
 const db = require("./config/db");
-const subscriberCtrl = require("./app/controllers/subscriberCtrl");
 const initRoutes = require("./app/routes");
 
 const port = 5000;
@@ -17,15 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 
 db.sequelize.sync();
 
-const dbConnect = async () => {
-  try {
-    await db.sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
-dbConnect();
+// const dbConnect = async () => {
+//   try {
+//     await db.sequelize.authenticate();
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// };
+// dbConnect();
 initRoutes(app);
 
 app.use("/", (req, res) => {
